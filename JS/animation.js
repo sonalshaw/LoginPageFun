5ccfa3a165495eb39468f49659c5e52ff3369172
Amit Chinara <amitchinara@gmail.com>
2024-05-30 20:29:03 +0530

    Added blinking mechanism

--- JS/animation.js
+++ JS/animation.js
@@ -1,28 +1,21 @@
-let leftEye = document.getElementById('left-eye');
-let rightEye = document.getElementById('right-eye');
-let inputField = document.getElementById('username');
+let eyeR = document.querySelector('#right-eye');
+let eyeL = document.querySelector('#left-eye');
 
-const updateEyePosition = () => {
-    let caretPosition = inputField.selectionStart;
-
-    // Define the original positions of the eyes
-    let originalLeftEyePositionX = 30;
-    let originalRightEyePositionX = 70;
-
-    // Define the range within which the eyes can move
-    let eyeMovementRange = 10;
-
-    // Calculate the new positions of the eyes based on the caret position
-    let newLeftEyePositionX = originalLeftEyePositionX + (caretPosition / inputField.value.length) * eyeMovementRange;
-    let newRightEyePositionX = originalRightEyePositionX + (caretPosition / inputField.value.length) * eyeMovementRange;
-
-    if (inputField.value.length == 0) {
-        newLeftEyePositionX = originalLeftEyePositionX;
-        newRightEyePositionX = originalRightEyePositionX;
+const startBlinking = (delay) => {
+    if(delay) {
+        delay = getRandomInt(delay);
+    } else {
+        delay = 1;
     }
-    // Update the positions of the eyes
-    leftEye.setAttribute('cx', String(newLeftEyePositionX));
-    rightEye.setAttribute('cx', String(newRightEyePositionX));
+    let blinking = TweenMax.to([eyeL, eyeR], .1, {
+        delay: delay, scaleY: 0, yoyo: true, repeat: 1, transformOrigin: "center center", onComplete: function () {
+            startBlinking(8);
+        }
+    });
 }
 
-inputField.addEventListener('input', updateEyePosition);
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+startBlinking(5);
