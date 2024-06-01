f7a8d2b6fcc7049cf35fb913df7b080b5642e999
Amit Chinara <amitchinara@gmail.com>
2024-06-01 15:32:57 +0530

    Added closing eye mechanism

--- JS/animation.js
+++ JS/animation.js
@@ -1,13 +1,17 @@
 let eyeR = document.querySelector('#right-eye');
 let eyeL = document.querySelector('#left-eye');
 
+let password = document.querySelector('#password');
+
+let blinking;
+
 const startBlinking = (delay) => {
     if(delay) {
         delay = getRandomInt(delay);
     } else {
         delay = 1;
     }
-    let blinking = TweenMax.to([eyeL, eyeR], .1, {
+    blinking = TweenMax.to([eyeL, eyeR], .1, {
         delay: delay, scaleY: 0, yoyo: true, repeat: 1, transformOrigin: "center center", onComplete: function () {
             startBlinking(8);
         }
@@ -18,4 +22,18 @@ const getRandomInt = (max) => {
     return Math.floor(Math.random() * Math.floor(max));
 }
 
-startBlinking(5);
+const closeEye = () => {
+    TweenMax.killTweensOf([eyeL, eyeR]);
+    TweenMax.to([eyeL, eyeR], .1, {scaleY: 0.1});
+}
+
+const openEyeL = () => {
+    TweenMax.killTweensOf(eyeL);
+    TweenMax.to(eyeL, .1, {scaleY: 0.7});
+}
+
+const initAnimation = () => {
+    startBlinking(5);
+}
+
+initAnimation();
